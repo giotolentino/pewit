55b6b39801bfada5e1df3ee62b7d59a2aaf1331d
Tapicargie <tapicargie7@gmail.com>
2023-04-11 16:44:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let a = 10
+let b = 20
+
+console.log(a == b);
+console.log(a != b);
+console.log(a < b);
+console.log(a > b);
+console.log(a <= b);
+console.log(a >= b);
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a ** b);
+// console.log(a % b);
+
+
+// prompt("eka mulala lage me ing lagyu mu!");
+
+// let num1 = "yokiee";
+// var num2 = "luse";
+// const  num3 = 2.34;
+// let x = "2.34";
+// let y =2.34 ;
+// let z = "";
+
+// x = Number(x)
+// y = String(y)
+// z = Boolean(z)
+
+// console.log(x,typeof x);
+// console.log(y,typeof y);
+// console.log(z,typeof z);
+
+// console.log(num1, typeof num1);
+// console.log(num2, typeof num2);
+// console.log(num3, typeof num3);
